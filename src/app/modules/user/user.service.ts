d4532a38ff328ce4574faebab27f6aeedd96cbce
Nazmul Hoque <sujon686500@gmail.com>
2024-12-05 01:00:48 +0600

    generateStudentId utility function added

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,29 +1,40 @@
 import config from '../../config';
+import { TAcademicSemester } from '../academic-semester/academicSemester.interface';
+import { AcademicSemester } from '../academic-semester/academicSemester.model';
 import { TStudent } from '../student/student.interface';
 import { Student } from '../student/student.model';
 import { TUser } from './user.interface';
 import { User } from './user.model';
+import { generateStudentId } from './user.utils';
 
-const createStudentIntoDB = async (password: string, studentData: TStudent) => {
+const createStudentIntoDB = async (password: string, payload: TStudent) => {
+  // create a user object
   const userData: Partial<TUser> = {};
 
+  //if password is not given , use deafult password
   userData.password = password || (config.default_password as string);
+
+  //set student role
   userData.role = 'student';
 
-  //set manually generated id
-  userData.id = '20301234';
+  // find academic semester info
+  const admissionSemester = await AcademicSemester.findById(
+    payload.admissionSemester,
+  );
 
-  //create a user
+  //set  generated id
+  userData.id = await generateStudentId(admissionSemester);
+
+  // create a user
   const newUser = await User.create(userData);
 
   //create a student
   if (Object.keys(newUser).length) {
-    // set id, _id as user
-    studentData.id = newUser.id; // embedding id
-    studentData.user = newUser._id; // reference _id
+    // set id , _id as user
+    payload.id = newUser.id;
+    payload.user = newUser._id; //reference _id
 
-    const newStudent = await Student.create(studentData);
-
+    const newStudent = await Student.create(payload);
     return newStudent;
   }
 };
